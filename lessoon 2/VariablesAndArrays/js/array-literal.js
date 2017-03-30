a7e82103c00ef0b29641a2925e5b9a89d962dd15
gary00796 <G00339880@gmit.ie>
2017-03-30 09:58:04 +0100

    Tester
    Testing new files to see if they work on Github

---
+++ lessoon 2/VariablesAndArrays/js/array-literal.js
@@ -0,0 +1,8 @@
+var colors = new Array('white'
+			'black'
+			'red');
+colours = ['white', 'black', 'red'];
+
+// Show the first item from the array.
+var el = document.getElementById('colors');
+el.textContent = colors[1];
